5289c2a98e6707b72ed20a47a25fe9dc93f42712
phyllers <phyllers@gmail.com>
2015-12-13 22:34:21 -0800

    removed trial code

--- shell/gcloud-pull-files.sh
+++ shell/gcloud-pull-files.sh
@@ -1,6 +1,4 @@
 /usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/udp.env" ./.env
-/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/bigquery_crednetials.dat" ./bigquery_crednetials.dat
-/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/client_secrets.json" ./client_secrets.json
 #./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/dev-ssl/demo01-client-cert.pem" ./client-cert.pem
 #./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/dev-ssl/demo01-client-key.pem" ./client-key.pem
 #./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/dev-ssl/demo01-server-ca.pem" ./server-ca.pem
