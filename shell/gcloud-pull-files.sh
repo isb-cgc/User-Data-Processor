295066fe9ada06f507372985b71cbebfcdf5611a
phyllers <plee@systemsbiology.org>
2015-12-14 16:48:32 -0800

    removed unused lines

--- shell/gcloud-pull-files.sh
+++ shell/gcloud-pull-files.sh
@@ -1,5 +1,1 @@
 /usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/udp.env" ./.env
-#./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/dev-ssl/demo01-client-cert.pem" ./client-cert.pem
-#./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/dev-ssl/demo01-client-key.pem" ./client-key.pem
-#./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/dev-ssl/demo01-server-ca.pem" ./server-ca.pem
-#./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/client_secrets.json" ./client_secrets.json
