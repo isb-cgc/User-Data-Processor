28aa1a92c466c15539d3bf0596bdab491cda70f5
phyllers <phyllers@gmail.com>
2016-01-21 14:41:27 -0800

    using dev database

--- shell/gcloud-pull-files.sh
+++ shell/gcloud-pull-files.sh
@@ -1,4 +1,4 @@
 /usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/udp.env" ./.env
-/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/lsdf-client-cert.pem" ./client-cert.pem
-/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/lsdf-client-key.pem" ./client-key.pem
-/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/lsdf-server-ca.pem" ./server-ca.pem
+/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/demo01-client-cert.pem" ./client-cert.pem
+/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/demo01-client-key.pem" ./client-key.pem
+/usr/bin/gsutil cp "gs://${GCLOUD_BUCKET}/udp-files/demo01-server-ca.pem" ./server-ca.pem
