e1124779ee8e5c917538bb5e6f8a9c0fc3eeea55
phyllers <phyllers@gmail.com>
2016-10-24 15:21:24 -0700

    test env var get url

--- user-data-processor.py
+++ user-data-processor.py
@@ -33,8 +33,8 @@ def generate_bq_schema(columns):
 
 
 def main(user_data_config, etl_config_file):
+    print os.environ.get('SUCCESS_URL')
     schemas_dir = os.path.join(os.getcwd(), 'schemas/')
-
     configs = open(user_data_config).read()
     data = json.loads(configs)
 
